5e5e422916fdcfdffed698be7661c9cd4ae3d964
Yera10 <Yera10@users.noreply.github.com>
2026-05-16 17:52:38 +0900

    fix : 영상페치방법 rss → YouTube Data API v3 로 전환
    rss_url 로 가져올 시, 막힘

--- fetch_videos.py
+++ fetch_videos.py
@@ -1,36 +1,60 @@
 import pandas as pd
-import feedparser
+import requests
 from sqlalchemy import create_engine, text
 from zoneinfo import ZoneInfo
+from dotenv import load_dotenv
+import os
+from pprint import pprint
+import html
 
 # DB CONNECT
 print(f"{'DB CONNECT':=^50}")
+load_dotenv()
+API_KEY = os.getenv("YOUTUBE_API_KEY")
 engine = create_engine("mariadb+mariadbconnector://root:1022@localhost:3306/LITTLEB")
 df = pd.read_sql("SELECT * FROM TRACKING_YOUTUBER", engine)
 print(df)
 
 kst = ZoneInfo("Asia/Seoul")
-the_time = (pd.Timestamp.now(tz=kst).tz_localize(None) - pd.Timedelta(hours=1)).floor('h')
+the_time = (pd.Timestamp.now(tz='UTC') - pd.Timedelta(hours=1)).floor('h').strftime('%Y-%m-%dT%H:%M:%SZ')
+
 # NEW VIDEOS
 print(f"{'NEW VIDEOS':=^50}")
 with engine.connect() as conn:
     for chid in df.CHANNEL_ID:
-        rss_url = f"https://www.youtube.com/feeds/videos.xml?channel_id={chid}"
-        feed = feedparser.parse(rss_url, agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36")
-        feed_df = pd.DataFrame(feed.entries)[['author', 'yt_videoid', 'link', 'title', 'published', 'updated']]
+        # API 요청
+        url = "https://www.googleapis.com/youtube/v3/search"
+        params = {
+            "key": API_KEY,
+            "channelId": chid,
+            "part": "snippet",
+            "order": "date",
+            "publishedAfter": the_time,
+            "maxResults": 50,
+            "type": "video",
+        }
+        resp = requests.get(url, params=params).json()
+        # pprint(resp)
 
-        # 시간 변환 및 필터링
-        for col in ['published', 'updated']:
-            feed_df[col] = pd.to_datetime(feed_df[col], utc=True).dt.tz_convert(kst).dt.tz_localize(None)
-        feed_df = feed_df[feed_df['published'] >= the_time]
-        feed_df['CHANNEL_ID'] = chid
-        print(feed_df)
+        # TO Dict
+        videos = []
+        for item in resp.get("items", []):
+            video_id = item["id"]["videoId"]
+            videos.append({
+                'CHANNEL_ID': item["snippet"]['channelId'],
+                "CHANNEL_TITLE":   item["snippet"]["channelTitle"],
+                "YT_VIDEOID": video_id,
+                "LINK":       f"https://www.youtube.com/watch?v={video_id}",
+                "TITLE":     html.unescape(item["snippet"]["title"]),
+                "PUBLISHED": pd.to_datetime(item["snippet"]["publishedAt"]).tz_convert(kst).strftime('%Y-%m-%d %H:%M:%S')
+            })
+        pprint(videos)
 
         # DB INSERT
-        if feed_df.empty:
+        if not videos:
             continue
         conn.execute(
-            text("INSERT IGNORE INTO TRACKING_VIDEOS (CHANNEL_ID, AUTHOR, YT_VIDEOID, LINK, TITLE, PUBLISHED, UPDATED) VALUES (:CHANNEL_ID, :author, :yt_videoid, :link, :title, :published, :updated)"),
-            feed_df.to_dict(orient='records')
+            text("INSERT IGNORE INTO TRACKING_VIDEOS (CHANNEL_ID, CHANNEL_TITLE, YT_VIDEOID, LINK, TITLE, PUBLISHED) VALUES (:CHANNEL_ID, :CHANNEL_TITLE, :YT_VIDEOID, :LINK, :TITLE, :PUBLISHED)"),
+            videos
         )
     conn.commit()
